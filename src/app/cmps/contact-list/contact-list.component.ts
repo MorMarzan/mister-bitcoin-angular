e87a6a4931403481a86b52ba61fb672735170685
Mor Marzan <mormarzan@gmail.com>
2024-02-21 21:32:42 +0200

    Implement remove and add style

--- src/app/cmps/contact-list/contact-list.component.ts
+++ src/app/cmps/contact-list/contact-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Contact } from '../../models/contact.model';
 @Component({
   selector: 'contact-list',
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
 export class ContactListComponent {
-
+  @Input() contacts: Contact[] | null = null
+  @Output() remove = new EventEmitter()
 }
