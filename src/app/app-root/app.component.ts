adab877872ab7185d93a42bdf916135589e30139
Mor Marzan <mormarzan@gmail.com>
2024-02-21 20:55:00 +0200

    Load contacts in app-root and change layout

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mister-bitcoin-angular';
+export class AppComponent implements OnInit {
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit(): void {
+    this.contactService.loadContacts()
+      .pipe(take(1))
+      .subscribe({
+        error: (error) => console.log('error:', error)
+      })
+  }
 }
